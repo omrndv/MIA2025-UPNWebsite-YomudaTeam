7083fcf65c9101f4706d8454524a41cfe4b9e73f
omrndv <umarnadiv@gmail.com>
2025-10-21 18:39:08 +0700

    push favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,11 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UMKM Banyumas</title>
+    <title>LokaMas - Website UMKM Lokal Banyumas</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
 </head>
 
 <body class="font-sans text-gray-800 bg-white">
